08b0d3235699ad1687db06affec8dd28002c3828
callo94 <info@mediaintegrati.it>
2021-03-19 17:25:48 +0100

    Sincronizzato!
    Battito sincronizzato!

--- Client/arduinoClient/arduinoClient.ino
+++ Client/arduinoClient/arduinoClient.ino
@@ -1,29 +1,30 @@
 /*
- Basic MQTT example
+  Basic MQTT example
 
- This sketch demonstrates the basic capabilities of the library.
- It connects to an MQTT server then:
+  This sketch demonstrates the basic capabilities of the library.
+  It connects to an MQTT server then:
   - publishes "hello world" to the topic "outTopic"
   - subscribes to the topic "inTopic", printing out any messages
     it receives. NB - it assumes the received payloads are strings not binary
 
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
- 
+  It will reconnect to the server if the connection is lost using a blocking
+  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+  achieve the same result without blocking the main loop.
+
 */
 
 #include <SPI.h>
-#include <Ethernet.h>
 #include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+
+const char* ssid = "Langino-low";
+const char* password = "pastadipizza";
 
 // Update these with values suitable for your network.
-byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
-IPAddress ip(192, 168, 1, 193);
-IPAddress server(192, 168, 1, 93);
+IPAddress server(167,71,79,43);
 
-//il pin che viene usato 
-int ledPin = 6;
+//il pin che viene usato
+int ledPin = 0;
 
 // Questa callback è esattamente la funzione che viene chiamata nel momento in cui riceve un messaggio
 void callback(char* topic, byte* payload, unsigned int length) {
@@ -31,8 +32,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
   pulsa();
 }
 
-EthernetClient ethClient;
-PubSubClient client(ethClient);
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
 
 void reconnect() {
   // Loop until we're reconnected
@@ -41,7 +42,7 @@ void reconnect() {
     // Attempt to connect
     if (client.connect("arduinoClient")) {
       Serial.println("connected");
-      // Una volta connesso si sottoscrive al topic 'main' 
+      // Una volta connesso si sottoscrive al topic 'main'
       client.subscribe("main");
     } else {
       Serial.print("failed, rc=");
@@ -55,12 +56,29 @@ void reconnect() {
 
 void setup()
 {
-  Serial.begin(57600);
-
+  Serial.begin(115200);
+  
   client.setServer(server, 1883);
   client.setCallback(callback);
+  
+  delay(10);
 
-  Ethernet.begin(mac, ip);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
   // Allow the hardware to sort itself out
   delay(1500);
 }
@@ -75,22 +93,22 @@ void loop()
 
 void pulsa() {
   delay(100);
-  
-  for (int i=0; i<255; i++){
+
+  for (int i = 0; i < 255; i++) {
     analogWrite(ledPin, i);
     delay(1);
   }
-  for (int i=255; i>0; i--){
+  for (int i = 255; i > 0; i--) {
     analogWrite(ledPin, i);
     delay(1);
   }
-  for (int i=0; i<255; i++){
+  for (int i = 0; i < 255; i++) {
     analogWrite(ledPin, i);
     delay(1);
   }
   delay(20);
-  
-  for (int i=255; i>=0; i--){
+
+  for (int i = 255; i >= 0; i--) {
     analogWrite(ledPin, i);
     delay(3);
   }
